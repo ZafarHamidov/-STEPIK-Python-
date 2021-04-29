0e70c40c622f05eedbd6ccfb789cabc05d8688a4
ZafarHamidov <50060143+ZafarHamidov@users.noreply.github.com>
2021-04-29 23:09:31 +0300

    Create 3.1.py

---
+++ 3.1.py
@@ -0,0 +1,38 @@
+"""
+/step/1
+print()
+
+/step/2
+print()
+print('Поэма "Мёртвые души" одна из самых интересных')
+print("3.1415")
+print("I'm a math teacher and a programmer!")
+
+/step/3
+print('Python', 'is the best', '!!')
+
+/step/4
+1*2*3*4
+
+/step/5
+print("Told you not to worry", "But maybe that's a lie", sep=' :) ')
+print("Honey, what's your hurry", end='?')
+print("The world's a little blurry", "Or maybe it's my eyes", end='!!!', sep=' :) ')
+
+/step/6
+input()
+
+/step/7
+n = int(input())
+
+/step/8
+Имя переменной не может совпадать с ключевым (зарезервированным) словом
+Имя переменной не может начинаться с цифры
+Имя переменной может начинаться с символа подчёркивания (_)
+
+/step/9
+60
+
+/step/10
+56
+"""
