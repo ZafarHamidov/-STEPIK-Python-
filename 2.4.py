99cb8a144b6a8d89b7d5da6793a0acac73092f7d
ZafarHamidov <50060143+ZafarHamidov@users.noreply.github.com>
2021-04-29 22:53:16 +0300

    Create 2.4.py

---
+++ 2.4.py
@@ -0,0 +1,72 @@
+"""
+/step/2
+  -50
+  
+/step/3
+  20
+  
+step/4
+  4 a
+  
+/step/5
+num = int(input())
+print(num + 0)
+print(num + 1)
+print(num + 2)
+
+/step/6
+num1 = int(input())
+num2 = int(input())
+num3 = int(input())
+print(num1 + num2 + num3)
+
+/step/7
+num = int(input())
+v = str(num ** 3)
+print("Объем = " + v)
+s = str(6 * (num ** 2))
+print("Площадь полной поверхности = " + s)
+
+/step/8
+a = int(input())
+b = int(input())
+f = str(3 * (a + b) ** 3 + 275 * b ** 2 - 127 * a - 41)
+print(f)
+
+/step/9
+a = int(input())
+a1 = str(a + 1)
+a2 = str(a - 1)
+print("Следующее за числом", a, "число: " + a1)
+print("Для числа", a, "предыдущее число: " + a2)
+
+/step/10
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+s = str((a + b + c + d) * 3)
+print(s)
+
+/step/11
+a = int(input())
+b = int(input())
+print(f"{a} + {b} = {a + b}")
+print(f"{a} - {b} = {a - b}")
+print(f"{a} * {b} = {a * b}")
+
+/step/12
+a1 = int(input())
+d = int(input())
+n = int(input())
+an = str(a1 + d * (n - 1))
+print(an)
+
+/step/13
+x = int(input())
+x2 = str(2 * x)
+x3 = str(3 * x)
+x4 = str(4 * x)
+x5 = str(5 * x)
+print(x, x2, x3, x4, x5, sep="---")
+"""
