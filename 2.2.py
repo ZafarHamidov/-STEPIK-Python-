c80c0a53b3d4dcf24e49fb832decdbb9464fad4a
ZafarHamidov <50060143+ZafarHamidov@users.noreply.github.com>
2021-04-29 22:39:49 +0300

    Update 2.2.py

--- 2.2.py
+++ 2.2.py
@@ -11,17 +11,17 @@
 #/step/4
   print("Здравствуй, мир!")
 """
-/step/5
+#/step/5
   print("4", "8", "15", "16", "23", "42")
-
-/step/6
+"""
+#/step/6
   print("4")
   print("8")
   print("15")
   print("16")
   print("23")
   print("42")
-
+"""
 /step/7
   print("*")
   print("**")
