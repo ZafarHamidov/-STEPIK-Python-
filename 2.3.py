2f5e82fc84a031c364833f3b65fb7353f63914fb
ZafarHamidov <50060143+ZafarHamidov@users.noreply.github.com>
2021-04-29 22:48:35 +0300

    Create 2.3.py

---
+++ 2.3.py
@@ -0,0 +1,81 @@
+"""
+/step/2
+Что будет выведено на экран в результате выполнения следующего кода?
+print('31', '12', '2019', sep='-')
+  * 31-12-2019
+  
+/step/3
+Что будет выведено на экран в результате выполнения следующего кода?
+print('Mercury', 'Venus', sep='*', end='!')
+print('Mars', 'Jupiter', sep='**', end='?')
+  * Mercury*Venus!Mars**Jupiter?
+  
+/step/4
+Сколько строк будет распечатано в результате выполнения следующего кода?
+print('a', 'b', 'c', sep='*')
+print('d', 'e', 'f', sep='**', end='')
+print('g', 'h', 'i', sep='+', end='%')
+print('j', 'k', 'l', sep='-', end='\n')
+print('m', 'n', 'o', sep='/', end='!')
+print('p', 'q', 'r', sep='1', end='%')
+print('s', 't', 'u', sep='&', end='\n')
+print('v', 'w', 'x', sep='%')
+print('y', 'z', sep='/', end='!')
+  * 5
+  
+/step/5
+print("I", "like", "Python", sep="***")
+
+step/6
+na = input()
+z = input()
+x = input()
+c = input()
+print(z, x, c, sep=na)
+
+/step/7
+ni = input()
+print("Привет, " + ni, end="!")
+
+/step/9
+Какие из имён допустимы для названия переменных в Python?
+  * teacher2
+  * teacher_2
+  * teacher
+  
+/step/10
+Что покажет приведенный ниже фрагмент кода?
+language = 'Python'
+language = 'Pascal'
+print(language)
+  * Pascal
+  
+/step/11
+Какое значение будет находиться в переменной s1 после выполнения следующего кода?
+
+s1 = 'C++'
+s2 = 'Python'
+s3 = 'Java'
+s3 = s2
+s1 = s3
+  * Python
+
+/step/13
+Выберите строки кода, соответствующие стандарту PEP 8.
+  * print('Follow PEP8!')
+  * print('Follow', 'PEP8!')
+  * print('Follow', 'PEP8', sep='**')
+  * name = input()
+
+/step/14
+Что будет напечатано в результате выполнения следующей программы?
+# print('Java')
+# print('Ruby')
+# print('Scala')
+print('Python', end='+')  # print('C++')
+# print('GO')
+print('C#', end='=') # print('C')
+print('awesome')
+# finish
+  * Python+C#=awesome
+"""
