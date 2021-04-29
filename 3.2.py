92a3263df9087f22f98e603f7b2b5244c0d60f8a
ZafarHamidov <50060143+ZafarHamidov@users.noreply.github.com>
2021-04-29 23:11:22 +0300

    Create 3.2.py

---
+++ 3.2.py
@@ -0,0 +1,30 @@
+"""
+/step/1
+print("*****************")
+print("*               *")
+print("*               *")
+print("*****************")
+
+/step/2
+a = int(input())
+b = int(input())
+ab2 = (a + b) ** 2
+a2b2 = (a ** 2) + (b ** 2)
+print("Квадрат суммы", a, "и", b, "равен", ab2)
+print("Сумма квадратов", a, "и", b, "равна", a2b2)
+
+/step/3
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+abcd = (a ** b) + (c ** d)
+print(abcd)
+
+/step/4
+a = int(input())
+b = a * 10 + a
+c = a * 100 + b
+print(a + b + c)
+
+"""
