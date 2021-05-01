d441c7a714713d7513072fb7e7af5a1c3e2620ef
ZafarHamidov <50060143+ZafarHamidov@users.noreply.github.com>
2021-05-02 00:28:11 +0300

    Create 4.2.py

---
+++ 4.2.py
@@ -0,0 +1,93 @@
+"""
+/step/2
+Логическое отрицание not
+Логическое умножение and
+Логическое сложение or
+
+/step/3
+True and False: False
+True and True: True
+False and True: False
+False and False: False
+True or False: True
+True or True: True
+False or True: True
+False or False: False
+not True: False
+not False: True
+
+/step/4
+a == 2 or b > 2: True
+6 <= c and a > 3: False
+1 != b and c != 3: True
+a >= -1 or a <= b: True
+not (a > 2): True
+not (c <= 10): False
+
+/step/5
+число 34 выиграло
+
+/step/6
+100
+
+/step/8
+x = int(input())
+if x > -1 and x < 17:
+    print("Принадлежит")
+else:
+    print("Не принадлежит")
+    
+/step/9
+x = int(input())
+if x <= -3 or x >= 7:
+    print("Принадлежит")
+else:
+    print("Не принадлежит")
+    
+/step/10
+x = int(input())
+if (x > -30 and x <= -2) or (x > 7 and x <= 25):
+    print("Принадлежит")
+else:
+    print("Не принадлежит")
+    
+/step/11
+x = int(input())
+if 1000 <= x <= 9999 and (x % 7 == 0 or x % 17 == 0):
+    print("YES")
+else:
+    print("NO")
+    
+/step/12
+a = int(input())
+b = int(input())
+c = int(input())
+if (a > b + c and b > c + a and c > a + b) or (a > b - c and b > c - a and c > a - b):
+    print("YES")
+else:
+    print("NO")
+    
+/step/13
+x = int(input())
+if (x % 4 == 0 and x % 100 != 0) or (x % 400 == 0):
+    print("YES")
+else:
+    print("NO")
+    
+/step/14
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+if (a == c) or (b == d):
+    print("YES")
+else:
+    print("NO")
+    
+/step/15
+a, b, c, d = int(input()), int(input()), int(input()), int(input())
+if -1 <= a - c <= 1 and -1 <= b - d <= 1:
+    print('YES')
+else:
+    print('NO')
+"""
