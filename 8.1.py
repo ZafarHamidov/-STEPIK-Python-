f6b59fc60e2a5ce437b9add2f1336792dbe38cfa
ZafarHamidov <50060143+ZafarHamidov@users.noreply.github.com>
2021-05-02 13:56:54 +0300

    Create 8.1.py

---
+++ 8.1.py
@@ -0,0 +1,53 @@
+"""
+/step/1
+0, 1, 2, 3, 4, 5
+
+/step/2
+2, 3, 4, 5
+
+/step/3
+0, 100, 200, 300, 400, 500
+
+/step/4
+10, 9, 8, 7, 6
+
+/step/5
+15 раз Python awesome! на каждой строке
+
+/step/6
+выводит количество чисел от 1 до n кратных 3, но не кратных 7
+
+/step/7
+n = int(input())
+total = 0
+for i in range(1, n + 1):
+total += i
+print(total)
+
+/step/8
+4
+
+/step/9
+выводит факториал числа n
+
+/step/10
+выводит минимальный делитель числа, отличный от единицы
+
+/step/11
+n = int(input())
+total = 0
+while n != 0:
+last_digit = n % 10
+total += last_digit
+n //= 10
+print(total)
+
+/step/12
+n = int(input())
+counter = 0
+while n > 0:
+counter += 1
+n //= 10
+print(counter)
+
+"""
