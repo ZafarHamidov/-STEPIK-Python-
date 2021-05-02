699f8464979a0da3a23cca2ecaaa9afb7c333dd2
ZafarHamidov <50060143+ZafarHamidov@users.noreply.github.com>
2021-05-02 13:14:23 +0300

    Create 7.1.py

---
+++ 7.1.py
@@ -0,0 +1,49 @@
+"""
+/step/2
+for i in range(10):
+    print("Python is awesome!")
+    
+/step/3
+a = input()
+b = int(input())
+for i in range(b):
+    print(a)
+
+/step/4
+for i in range(6):
+    print("AAA")
+for i in range(5):
+    print("BBBB")
+print("E")
+for i in range(9):
+    print("TTTTT")
+print("G")
+
+/step/5
+n = int(input())
+for i in range(n):
+    print("*" * 19)
+    
+/step/7
+a = input()
+for i in range(10):
+    print(i, a)
+    
+/step/8
+n = int(input())
+for i in range(n + 1):
+    print("Квадрат числа", i, "равен", i * (i))
+    
+/step/9
+n = int(input())
+for i in range(n):
+    print((n - i) * "*")
+    
+/step/10
+m, p, n = int(input()), int(input()) * 0.01 + 1, int(input())
+for i in range(n):
+    print(i + 1, m)
+    m = m * p
+
+
+"""
