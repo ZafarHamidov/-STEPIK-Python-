34ad4ad47ab20fe8467144ee035b3193dcf4a762
ZafarHamidov <50060143+ZafarHamidov@users.noreply.github.com>
2021-05-02 13:36:53 +0300

    Create 7.6.py

---
+++ 7.6.py
@@ -0,0 +1,38 @@
+"""
+/step/2
+0*1*2*3*4*5*6*7*
+
+/step/3
+100*80*60*
+
+/step/4
+9*8*7*6*5*4*3*1*0*
+
+/step/5
+25
+
+/step/6
+105
+
+/step/7
+n = int(input())
+i = 2
+while n % i != 0:
+    i += 1
+print(i)
+
+/step/8
+nn = int(input())
+for i in range(1, nn + 1):
+    if i < 5 or 9 < i < 17 or 37 < i < 78 or i > 87:
+        print(i)
+
+/step/10
+Да
+
+/step/11
+Нет
+
+/step/12
+Да
+"""
