31beb5fa15a005a3728db3be6488fad7a5971215
ZafarHamidov <50060143+ZafarHamidov@users.noreply.github.com>
2021-05-02 13:44:27 +0300

    Create 7.8.py

---
+++ 7.8.py
@@ -0,0 +1,68 @@
+"""
+/step/2
+1 3
+1 4
+1 5
+2 3
+2 4
+2 5
+3 3
+3 4
+3 5
+
+/step/3
+455667
+
+/step/4
+8
+
+/step/5
+n = int(input())
+for i in range(n):
+    for j in range(3):
+        print(n, end=' ')
+    print()
+    
+/step/6
+n = int(input())
+
+for i in range(n):
+    print((str(i+1) + ' ')*5)
+    
+/step/7
+n = int(input())
+
+for i in range(1, n+1):
+    for j in range(1, 10):
+        print('{0} + {1} = {2}'.format(i, j, i+j))
+    print()
+    
+/step/8
+n = int(input())
+for i in range(1, n + 1):
+    print('*' * min(i, n - i + 1))
+    
+/step/9
+n = int(input())
+
+for i in range(1, n+1):
+    print(str(i)*i)
+    
+/step/11
+n = 1
+k = 4
+m = 7
+
+или
+n = 2
+k = 1
+m = 9
+
+/step/12
+Количество быков: 1
+Количество коров: 9
+Количество телят: 90
+
+/step/13
+498
+"""
