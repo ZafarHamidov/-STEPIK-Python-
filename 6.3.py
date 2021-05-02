d2b4c38be504d7f3bedab761946c3767c83a8480
ZafarHamidov <50060143+ZafarHamidov@users.noreply.github.com>
2021-05-02 13:10:13 +0300

    Create 6.3.py

---
+++ 6.3.py
@@ -0,0 +1,84 @@
+"""
+/step/2
+данетда
+
+/step/3
+121312141213121
+
+/step/4
+123123123456456789
+
+/step/5
+print('''"Python is a great language!", said Fred. "I don't ever remember having this much fun before."''')
+
+/step/6
+firstname = input()
+lastname = input()
+print("Hello " + firstname, lastname + "! You just delved into Python")
+
+/step/7
+a = input()
+c = str(len(a))
+print("Футбольная команда " + a + " имеет длину " + c + " символов")
+
+/step/8
+a = input()
+b = input()
+c = input()
+z = len(a)
+x = len(b)
+v = len(c)
+d = max(z, x, v)
+f = min(z, x, v)
+
+if f == z:
+    print(a)
+elif f == x:
+    print(b)
+elif f == v:
+    print(c)
+
+if d == z:
+    print(a)
+elif d == x:
+    print(b)
+elif d == v:
+    print(c)
+    
+/step/9
+a = len(input())
+b = len(input())
+c = len(input())
+if a + b + c == (min(a, b, c) + max(a, b, c))/2*3:
+    print("YES")
+else:
+    print("NO")
+    
+/step/11
+s = '3ab'
+s = '23'
+s = '123abc'
+s = '1'
+s = 'a'
+
+/step/12
+n = input()
+if "синий" in n:
+    print("YES")
+else:
+    print("NO")
+    
+/step/13
+n = input()
+if "суббота" in n or "воскресенье" in n:
+    print("YES")
+else:
+    print("NO")
+    
+/step/14
+a = str(input())
+if "@" in a and "." in a:
+    print("YES")
+else:
+    print("NO")
+"""
