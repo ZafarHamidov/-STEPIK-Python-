0fd1cf534e531972de0131f91744b47946dd6fd1
ZafarHamidov <50060143+ZafarHamidov@users.noreply.github.com>
2021-05-07 23:27:47 +0300

    Create 11.2.py

---
+++ 11.2.py
@@ -0,0 +1,38 @@
+"""
+/step/2
+primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71]
+print(primes[16])
+
+/step/3
+primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71]
+print(primes[-1])
+
+/step/4
+primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71]
+print(primes[:6])
+
+/step/5
+numbers = [12.5, 3.1415, 2.718, 9.8, 1.414, 1.1618, 1.324]
+print(max(numbers) + min(numbers))
+
+/step/6
+evens = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+average = sum(evens) / len(evens)
+print(average)
+
+/step/7
+rainbow = ['Red', 'Orange', 'Yellow', 'Green', 'Blue', 'Indigo', 'Violet']
+rainbow[3] = 'Зеленый'
+rainbow[6] = 'Фиолетовый'
+print(rainbow)
+
+/step/8
+languages = ['Chinese', 'Spanish', 'English', 'Hindi', 'Arabic', 'Bengali', 'Portuguese', 'Russian', 'Japanese', 'Lahnda']
+print(languages[::-1])
+
+/step/9
+numbers1 = [1, 2, 3]
+numbers2 = [6]
+numbers3 = [7, 8, 9, 10, 11, 12, 13]
+print(numbers1 * 2 + numbers2 * 9 + numbers3)
+"""
