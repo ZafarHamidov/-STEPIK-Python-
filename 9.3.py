8e75b9f587fa7f2795819d913745293cd05e8a66
ZafarHamidov <50060143+ZafarHamidov@users.noreply.github.com>
2021-05-07 13:06:56 +0300

    Create 9.3.py

---
+++ 9.3.py
@@ -0,0 +1,47 @@
+"""
+/step/2
+I learn python language
+
+/step/3
+i learn python language
+
+/step/4
+$12344%^$#@!
+
+/step/5
+a A
+
+/step/6
+I LEARN PYTHON LANGUAGE
+
+/step/7
+I learn pYTHON laNGUAge
+
+/step/8
+a = input()
+if a.title() == a:
+    print("YES")
+else:
+    print("NO")
+    
+/step/9
+n = input()
+print(n.swapcase())
+
+/step/10
+n = input()
+if "хорош" in n.lower():
+    print("YES")
+else:
+    print("NO")
+    
+/step/11
+s = input()
+counter = 0
+
+for i in s:
+    if i.islower():
+        counter += 1
+        
+print(counter)
+"""
