7ff5d9dca32a6f56763760b92a32816601cde939
ZafarHamidov <50060143+ZafarHamidov@users.noreply.github.com>
2021-05-07 13:12:10 +0300

    Create 9.5.py

---
+++ 9.5.py
@@ -0,0 +1,25 @@
+"""
+/step/2
+True
+False
+False
+
+/step/3
+True
+
+/step/4
+False
+
+/step/5
+False
+
+/step/7
+s = 'In {0}, someone paid {1} {2} for two pizzas.'
+print(s.format('2010', '10k', 'Bitcoin'))
+
+/step/8
+year = 2010
+amount = '10K'
+currency = 'Bitcoin'
+print(f'In {year}, someone paid {amount} {currency} for two pizzas.')
+"""
