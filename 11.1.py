476c583d66f363e6d4220020fc07592bc38f1d8c
ZafarHamidov <50060143+ZafarHamidov@users.noreply.github.com>
2021-05-07 23:25:10 +0300

    Create 11.1.py

---
+++ 11.1.py
@@ -0,0 +1,28 @@
+"""
+/step/2
+элементами
+
+/step/3
+4
+
+/step/4
+4
+
+/step/5
+да
+
+/step/6
+[0, 1, 3, 14, 2, 7, 9, 8, 10]
+
+/step/7
+['Michael', 'John', 'Freddie']
+
+/step/8
+n = int(input())
+print(list(range(1, n+1)))
+
+/step/9
+n = int(input())
+s = 'abcdefghijklmnopqrstuvwxyz'
+print(list(s[:n]))
+"""
