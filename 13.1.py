8347ed95446c8ee11f8d14e94e918f0fce757832
ZafarHamidov <50060143+ZafarHamidov@users.noreply.github.com>
2021-05-08 00:02:52 +0300

    Create 13.1.py

---
+++ 13.1.py
@@ -0,0 +1,42 @@
+"""
+/step/2
+функцией
+
+/step/3
+заголовком
+
+/step/4
+телом
+
+/step/5
+вызывают
+
+/step/6
+is_valid
+print_numbers
+find_sum_1
+_myfunction
+draw_triangle
+
+/step/7
+для создания заглушки
+
+/step/8
+# объявление функции
+def draw_box():
+    print("**********")
+    for i in range(12):
+        print("*        *")
+    print("**********")
+
+# основная программа
+draw_box()  # вызов функции
+
+/step/9
+# объявление функции
+def draw_triangle():
+    for i in range(1,11):
+        print(i * '*')
+
+draw_triangle()  # вызов функции
+"""
