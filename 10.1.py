e16ff1536362950a88d97bc9f7ec5eda79964e87
ZafarHamidov <50060143+ZafarHamidov@users.noreply.github.com>
2021-05-07 13:16:01 +0300

    Create 10.1.py

---
+++ 10.1.py
@@ -0,0 +1,33 @@
+"""
+/step/1
+0
+
+/step/2
+длина строки минус один
+
+/step/3
+произойдет ошибка IndexError
+
+/step/4
+in
+
+/step/5
+find()
+
+/step/6
+lstrip()
+
+/step/7
+strip()
+
+/step/8
+isalpha()
+
+/step/9
+isdigit()
+
+/step/10
+джо
+ДЖО
+джо
+"""
